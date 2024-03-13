b04930becc588f8b340ffa9375e9ac6cb93c2f5e
shriyakalakata <shriyakalakata123@gmail.com>
2024-03-13 16:19:12 -0400

    Add test_dancer.py

---
+++ tests/test_dancer.py
@@ -0,0 +1,30 @@
+import pytest
+from pydancer import dancer
+
+class Tests:
+
+    @pytest.fixture
+    def example_fixture(self):
+        """
+        An example of a pytest fixture - a function that can be used for setup and teardown before and after test functions are run.
+        """
+
+        # place any setup you want to do before any test function that uses this fixture is run
+
+        yield  # at th=e yield point, the test function will run and do its business
+
+        # place with any teardown you want to do after any test function that uses this fixture has completed
+
+    #
+    # Test functions
+    #
+
+    def test_sanity_check(self, example_fixture):
+        """
+        Test debugging... making sure that we can run a simple test that always passes.
+        Note the use of the example_fixture in the parameter list - any setup and teardown in that fixture will be run before and after this test function executes
+        From the main project directory, run the `python3 -m pytest` command to run all tests.
+        """
+        expected = True  # the value we expect to be present
+        actual = True  # the value we see in reality
+        assert actual == expected, "Expected True to be equal to True!"
