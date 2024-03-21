0beb7be0327b9b6774768e01346fcb4700eedd93
al6862 <al6862@nyu.edu>
2024-03-21 10:04:00 -0400

    added argument parser + songs function

--- src/pydancer/__main__.py
+++ src/pydancer/__main__.py
@@ -1,7 +1,24 @@
-import pydancer.dancer as dancer
+import argparse
+import pydancer.songs as songs
+# import pydancer.dancer as dancer
+# use this as reference for argument parser: https://github.com/grantjenks/free-python-games/blob/master/src/freegames/__main__.py
 
 def main():
-    print("") # temporary
+    parser = argparse.ArgumentParser(
+        prog="pydancer",
+        description="Rhythm Game on Python",
+        epilog="Made by Speed"
+    )
+    subparsers = parser.add_subparsers(dest='command', help='sub-command help', required=True)
+
+    songsParser = subparsers.add_parser('songs', help='list available songs')
+    songsParser.add_argument('genre', choices=['country', 'pop'], help='genre name')
+
+    args = parser.parse_args()
+    if args.command == 'songs':
+        songs.listSongs(args.genre)
+
+
 
 if __name__ == "__main__":
     main()
---
+++ src/pydancer/songs.py
@@ -0,0 +1,9 @@
+# prob want this to just read the titles of song files instead of hard coded?
+def listSongs(genre):
+    print("genre:", genre)
+    if genre == 'country':
+        print('country song 1')
+        print('country song 2')
+    if genre == 'pop':
+        print('pop song 1')
+        print('pop song 2')
