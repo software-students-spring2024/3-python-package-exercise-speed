e43e400bb5178d24e44d1875f5bf62ba4995348f
gboeker <glenda.boeker@gmail.com>
2024-03-23 19:29:01 -0400

    gave up on organizing into files because of import error

--- src/pydancer/dancer.py
+++ src/pydancer/dancer.py
@@ -1,6 +1,24 @@
 import pygame
-from .constants import * 
-from .functions import * 
+# from .constants import * 
+# from .functions import * 
+
+# Constants bc I can't fix import errors
+GAME_NAME = "PyDancer"
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+BACKGROUND_COLOR = (250, 250, 250)
+TIMEOUT_DURATION = 30000  # 30 seconds
+
+# image functions because I can't fix import errors
+def load_image(image_path):
+    return pygame.image.load(image_path)
+
+def display_image(screen, image, x, y):
+    screen.blit(image, (x, y))
+
+def scale_image(image, x, y):
+    return pygame.transform.scale(image, (x, y))
+
+
 
 def play():
     pygame.init()
@@ -37,4 +55,3 @@ def play():
 
     pygame.quit()
 
-play()
