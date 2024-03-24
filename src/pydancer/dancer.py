245a39b134fac4d05f8df4b91a61f1cc5580c237
iltenahmet <iltenahmet@gmail.com>
2024-03-24 17:53:49 -0400

    add speed field, refactor display code

--- src/pydancer/dancer.py
+++ src/pydancer/dancer.py
@@ -10,6 +10,8 @@ class Pos:
         self.y = y
 
 class Component:
+    speed = 0
+
     def __init__(self, image, pos):
         self.image = image
         self.pos = pos
@@ -22,6 +24,10 @@ class Component:
     def get_draw_y(self):
         return self.pos.y - (self.image.get_height() / 2)
 
+    def display(self, screen):
+        display_image(screen, self.image, self.get_draw_x(), self.get_draw_y())
+
+
 def play():
     pygame.init()
 
@@ -29,29 +35,38 @@ def play():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption(GAME_NAME)
     clock = pygame.time.Clock()
-    last_event_time = pygame.time.get_ticks()
+    # last_event_time = pygame.time.get_ticks()
 
     # set up components 
     dancer_image = load_image("../images/dancer.png")
     dancer_image = scale_image(dancer_image, DANCER_WIDTH, DANCER_HEIGHT)
-    dancer = Component(dancer_image, Pos(SCREEN_WIDTH * .4 , SCREEN_HEIGHT * .1))
+    dancer = Component(dancer_image, Pos(SCREEN_WIDTH * .5 , SCREEN_HEIGHT * .1))
+
+    end_area_image = load_image("../images/end_area.png")
+    end_area_image = scale_image(end_area_image, SCREEN_WIDTH, SCREEN_HEIGHT * .2)
+    end_area = Component(end_area_image, Pos(SCREEN_WIDTH * .5, SCREEN_HEIGHT * .3))
 
     up_arrow_image = load_image("../images/up_arrow_filled.png")
     up_arrow_image = scale_image(up_arrow_image, ARROW_WIDTH, ARROW_HEIGHT)
     up_arrow = Component(up_arrow_image, Pos(SCREEN_WIDTH * .4, SCREEN_HEIGHT * .9))
+    up_arrow.speed = 50
 
     down_arrow_image = load_image("../images/down_arrow_filled.png")
     down_arrow_image = scale_image(down_arrow_image, ARROW_WIDTH, ARROW_HEIGHT)
     down_arrow = Component(down_arrow_image, Pos(SCREEN_WIDTH * .6, SCREEN_HEIGHT * .9))
+    down_arrow.speed = 90
 
     left_arrow_image = load_image("../images/left_arrow_filled.png")
     left_arrow_image = scale_image(left_arrow_image, ARROW_WIDTH, ARROW_HEIGHT)
     left_arrow = Component(left_arrow_image, Pos(SCREEN_WIDTH * .2, SCREEN_HEIGHT * .9))
+    left_arrow.speed = 40
 
     right_arrow_image = load_image("../images/right_arrow_filled.png")
     right_arrow_image = scale_image(right_arrow_image, ARROW_WIDTH, ARROW_HEIGHT)
     right_arrow = Component(right_arrow_image, Pos(SCREEN_WIDTH * .8, SCREEN_HEIGHT * .9))
+    right_arrow.speed = 45
 
+    # Game Loop
     running = True
     while running:
         # Check for events
@@ -59,22 +74,25 @@ def play():
             if event.type == pygame.QUIT:
                 running = False
 
-
         # delta time is needed to make updates independent of the frame rate
         delta_time = clock.tick(FPS)/1000
 
-        up_arrow.pos.y -= 20 * delta_time
-        down_arrow.pos.y -= 20 * delta_time
-        left_arrow.pos.y -= 20 * delta_time
-        right_arrow.pos.y -= 20 * delta_time
+        # update arrow positions
+        up_arrow.pos.y -= up_arrow.speed * delta_time
+        down_arrow.pos.y -= down_arrow.speed * delta_time
+        left_arrow.pos.y -= left_arrow.speed * delta_time
+        right_arrow.pos.y -= right_arrow.speed * delta_time
 
         # Draw screen
         screen.fill(BACKGROUND_COLOR)
-        display_image(screen, dancer.image, dancer.pos.x, dancer.pos.y)
-        display_image(screen, up_arrow.image, up_arrow.get_draw_x(), up_arrow.pos.y)
-        display_image(screen, down_arrow.image, down_arrow.get_draw_x(), down_arrow.pos.y)
-        display_image(screen, left_arrow.image, left_arrow.get_draw_x(), left_arrow.pos.y)
-        display_image(screen, right_arrow.image, right_arrow.get_draw_x(), right_arrow.pos.y)
+
+        # display components
+        end_area.display(screen)
+        up_arrow.display(screen)
+        down_arrow.display(screen)
+        right_arrow.display(screen)
+        left_arrow.display(screen)
+        dancer.display(screen)
 
         pygame.display.flip()
 
