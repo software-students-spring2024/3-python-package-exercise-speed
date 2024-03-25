80d44a0f37a85be360f529b636e1e9eb56739679
gboeker <glenda.boeker@gmail.com>
2024-03-25 18:22:42 -0400

    temporary fix for import test issue

--- src/pydancer/dancer.py
+++ src/pydancer/dancer.py
@@ -8,6 +8,26 @@ environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
 
 def play():
+    # moved images here bc of testing import error
+    left_glow_arrow_image = load_image("../images/left_arrow_glow.png")
+    left_glow_arrow_image = scale_image(left_glow_arrow_image, ARROW_WIDTH, ARROW_HEIGHT)
+
+    right_glow_arrow_image = load_image("../images/right_arrow_glow.png")
+    right_glow_arrow_image = scale_image(right_glow_arrow_image, ARROW_WIDTH, ARROW_HEIGHT)
+
+    up_glow_arrow_image = load_image("../images/up_arrow_glow.png")
+    up_glow_arrow_image = scale_image(up_glow_arrow_image, ARROW_WIDTH, ARROW_HEIGHT)
+
+    down_glow_arrow_image = load_image("../images/down_arrow_glow.png")
+    down_glow_arrow_image = scale_image(down_glow_arrow_image, ARROW_WIDTH, ARROW_HEIGHT)
+    # dancer images
+    dancer_image = load_image("../images/dancer.png")
+    dancer_image = scale_image(dancer_image, DANCER_WIDTH, DANCER_HEIGHT)
+    # purple end area image
+    end_area_image = load_image("../images/end_area.png")
+    end_area_image = scale_image(end_area_image, SCREEN_WIDTH, SCREEN_HEIGHT * .2)
+
+
     pygame.init()
 
     # Pygame setup
@@ -35,6 +55,7 @@ def play():
             if event.type == pygame.QUIT:
                 running = False 
 
+            # TODO: FOR SOME REASON THE RIGHT AND UP ARROWS ARE NOT WORKING, left and down are good
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     # NOTE: You can check if an arrow is in the target area with
