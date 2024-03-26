af70a575bf86216d7558bf560244ec97a0767cc0
iltenahmet <iltenahmet@gmail.com>
2024-03-26 14:42:25 -0400

    remove manual arrow image code

--- src/pydancer/dancer.py
+++ src/pydancer/dancer.py
@@ -8,20 +8,6 @@ environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
 
 def play():
-    '''
-    # moved images here bc of testing import error
-    left_glow_arrow_image = load_image("../images/left_arrow_glow.png")
-    left_glow_arrow_image = scale_image(left_glow_arrow_image, ARROW_WIDTH, ARROW_HEIGHT)
-
-    right_glow_arrow_image = load_image("../images/right_arrow_glow.png")
-    right_glow_arrow_image = scale_image(right_glow_arrow_image, ARROW_WIDTH, ARROW_HEIGHT)
-
-    up_glow_arrow_image = load_image("../images/up_arrow_glow.png")
-    up_glow_arrow_image = scale_image(up_glow_arrow_image, ARROW_WIDTH, ARROW_HEIGHT)
-
-    down_glow_arrow_image = load_image("../images/down_arrow_glow.png")
-    down_glow_arrow_image = scale_image(down_glow_arrow_image, ARROW_WIDTH, ARROW_HEIGHT)
-    '''
     # dancer images
     dancer_image = load_image("../images/dancer.png")
     dancer_image = scale_image(dancer_image, DANCER_WIDTH, DANCER_HEIGHT)
