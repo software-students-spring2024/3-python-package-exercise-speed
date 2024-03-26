faee9d8b7d236fa6f79fe8a7a57b78ce555f81f3
iltenahmet <iltenahmet@gmail.com>
2024-03-26 14:53:33 -0400

    remove manual arrow image paths

--- src/pydancer/dancer.py
+++ src/pydancer/dancer.py
@@ -6,17 +6,7 @@ from .component import *
 from .images import *
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
-
 def play():
-    # dancer images
-    dancer_image = load_image("../images/dancer.png")
-    dancer_image = scale_image(dancer_image, DANCER_WIDTH, DANCER_HEIGHT)
-    # purple end area image
-    end_area_image = load_image("../images/end_area.png")
-    end_area_image = scale_image(end_area_image, SCREEN_WIDTH, SCREEN_HEIGHT * .2)
-
-
-
     pygame.init()
 
     # Pygame setup
