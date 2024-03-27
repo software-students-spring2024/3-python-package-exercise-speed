9878043758fe2e5e57f540c77daf8bb7c5bfb44c
gboeker <glenda.boeker@gmail.com>
2024-03-27 16:29:45 -0400

    add basic tests + cleanup load_music function

--- src/pydancer/dancer.py
+++ src/pydancer/dancer.py
@@ -87,13 +87,7 @@ def load_music(song):
     # TO-DO: add more songs
     # TO-DO: Change milliseconds variable based on song to sync with the beat
     # TO-DO: possibly load song from a database/JSON file that has milliseconds info etc
-    if song == "blood-and-steel":
-        print("here")
-        pygame.mixer.music.load("../static/music/blood-and-steel.mp3")
-    elif song == "two-in-the-rain":
-        pygame.mixer.music.load("../static/music/two-in-the-rain.mp3")
-    else:
-        pygame.mixer.music.load("../static/music/test.mp3")
+    pygame.mixer.music.load("../static/music/" + song + ".mp3")
     pygame.mixer.music.play(1) # Play the music once
 
 def set_difficulty(difficulty):
