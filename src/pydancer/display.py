cffaab559f786a5ccb8c97e1f0f0878f7d3da20a
al6862 <al6862@nyu.edu>
2024-03-27 12:28:18 -0400

    updated display options

--- src/pydancer/display.py
+++
@@ -1,9 +0,0 @@
-def listDisplay(args): 
-    if args.characters and args.themes:
-        return 'Characters:\ngirl character\nboy character\nThemes:\npink\nblue'
-    elif args.characters:
-        return 'Characters:\ngirl character\nboy character'
-    elif args.themes:
-        return 'Themes:\npink\nblue'
-    else:
-        return 'Characters:\ngirl character\nboy character\nThemes:\npink\nblue'
